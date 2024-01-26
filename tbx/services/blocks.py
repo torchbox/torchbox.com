c91aec27cdc4a691d63acc4cc66699731d742308
Lauren <lauren.parsons@torchbox.com>
2024-01-26 10:16:44 +0000

    Initialise services app

---
+++ tbx/services/blocks.py
@@ -0,0 +1,6 @@
+from tbx.core.blocks import StoryBlock
+
+
+class ServiceStoryBlock(StoryBlock):
+    # Dev note - include partners_block here when merged,
+    pass
---
+++ tbx/services/models.py
@@ -0,0 +1,28 @@
+from tbx.core.utils.models import SocialFields
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.fields import RichTextField, StreamField
+from wagtail.models import Page
+from wagtail.search import index
+
+from .blocks import ServiceStoryBlock
+
+
+class ServicePage(SocialFields, Page):
+    template = "patterns/pages/service/service_page.html"
+
+    intro = RichTextField(blank=True)
+    body = StreamField(ServiceStoryBlock(), use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+        FieldPanel("body"),
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        MultiFieldPanel(SocialFields.promote_panels, "Social fields"),
+    ]
+
+    search_fields = Page.search_fields + [
+        index.SearchField("body"),
+    ]
