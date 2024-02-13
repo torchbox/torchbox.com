3927af961d6cfcd77debd69d0b34d2133b0c584f
Helen C <helen.chapman@torchbox.com>
2024-02-13 09:56:44 +0000

    Update stylelint config to allow dvh units

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
     // See https://github.com/torchbox/stylelint-config-torchbox for rules.
     extends: 'stylelint-config-torchbox',
+    rules: {
+        'unit-no-unknown': [
+            true,
+            {
+                ignoreUnits: ['dvh'],
+            },
+        ],
+    },
 };
