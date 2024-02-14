32476a584215a5c9bb5d7d72efc1c077d0c786fe
Helen C <helen.chapman@torchbox.com>
2024-02-14 09:51:46 +0000

    Person page FE

--- tbx/project_styleguide/templatetags/wagtailimages_tags.py
+++ tbx/project_styleguide/templatetags/wagtailimages_tags.py
@@ -2,3 +2,4 @@ from pattern_library.monkey_utils import override_tag
 from wagtail.images.templatetags.wagtailimages_tags import register
 
 override_tag(register, name="image", default_html="")
+override_tag(register, name="srcset_image", default_html="")
