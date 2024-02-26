7ca067a06b8a68a8eaa751805d2639d0812de309
Helen C <helen.chapman@torchbox.com>
2024-02-26 09:49:58 +0000

    Tabbed paragraph block

--- tbx/static_src/javascript/components/tabs.js
+++ tbx/static_src/javascript/components/tabs.js
@@ -1,31 +1,3 @@
-// Assumes the following html structure
-// See patterns/components/tabs-nav/tabs.html in the project styleguide
-/* <div class="tabs" data-tab-set>
-    <div class="tabs-nav" aria-label="Tabs">
-        <div class="tabs-nav__layout grid">
-            <div class="tabs-nav__container" role="tablist" aria-label="Tabs">
-
-                <a id="tab-1" class="tab-nav-item tab-nav-item--active" href="#tab-tab-1" role="tab" aria-controls="tab-1" data-tab="tab-tab-1" aria-selected="true">
-                    Tab 1
-                </a>
-
-
-                <a id="tab-2" class="tab-nav-item " href="#tab-tab-2" role="tab" aria-controls="tab-2" data-tab="tab-tab-2" aria-selected="false">
-                    Tab 2
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="tabs__panel" id="tab-tab-1" role="tabpanel" aria-labelledby="tab-1" data-tab-panel>
-        <p>Test one</p>
-    </div>
-
-    <div class="tabs__panel tabs__panel--hidden" id="tab-tab-2" role="tabpanel" aria-labelledby="tab-2" data-tab-panel>
-        <p>Test two</p>
-    </div>
-</div> */
-
 class Tabs {
     static selector() {
         return '[data-tab-set]';
@@ -35,7 +7,7 @@ class Tabs {
         this.tabset = node;
         this.allTabs = this.tabset.querySelectorAll('[data-tab]');
         this.allTabPanels = this.tabset.querySelectorAll('[data-tab-panel]');
-        this.activeTabClass = 'tab-nav-item--active';
+        this.activeTabClass = 'tabs__nav-item--active';
         this.inactiveTabPaneClass = 'tabs__panel--hidden';
         this.updateHistory = false; // change to true if you would like a tab change to update the url and page history
         this.setActiveHashTab();
