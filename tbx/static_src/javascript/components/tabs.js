cbdb90d37fe84b0f855fb866a62df62775e5c820
Helen Chapman <helen.chapman@torchbox.com>
2024-02-29 08:21:33 +0000

    Tabbed paragraph block (#123)
    * Tabbed paragraph block fe

--- tbx/static_src/javascript/components/tabs.js
+++ tbx/static_src/javascript/components/tabs.js
@@ -1,31 +1,3 @@
-// Assumes the following html structure
-// See patterns/components/tabs-nav/tabs.html in the project styleguide
-/* <div class="tabs" data-tab-set>
-    <div class="tabs-nav" aria-label="Tabs">
-        <div class="tabs-nav__layout grid">
-            <div class="tabs-nav__container" role="tablist" aria-label="Tabs">
-
-                <a id="tab-1" class="tab-nav-item tab-nav-item--active" href="#tab-tab-1" role="tab" aria-controls="tab-1" data-tab="tab-tab-1" aria-selected="true">
-                    Tab 1
-                </a>
-
-
-                <a id="tab-2" class="tab-nav-item " href="#tab-tab-2" role="tab" aria-controls="tab-2" data-tab="tab-tab-2" aria-selected="false">
-                    Tab 2
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="tabs__panel" id="tab-tab-1" role="tabpanel" aria-labelledby="tab-1" data-tab-panel>
-        <p>Test one</p>
-    </div>
-
-    <div class="tabs__panel tabs__panel--hidden" id="tab-tab-2" role="tabpanel" aria-labelledby="tab-2" data-tab-panel>
-        <p>Test two</p>
-    </div>
-</div> */
-
 class Tabs {
     static selector() {
         return '[data-tab-set]';
@@ -35,9 +7,10 @@ class Tabs {
         this.tabset = node;
         this.allTabs = this.tabset.querySelectorAll('[data-tab]');
         this.allTabPanels = this.tabset.querySelectorAll('[data-tab-panel]');
-        this.activeTabClass = 'tab-nav-item--active';
+        this.activeTabClass = 'tabs__nav-item--active';
         this.inactiveTabPaneClass = 'tabs__panel--hidden';
-        this.updateHistory = false; // change to true if you would like a tab change to update the url and page history
+        this.updateHistory = true; // change to true if you would like a tab change to update the url and page history
+
         this.setActiveHashTab();
 
         this.bindEvents();
@@ -57,9 +30,9 @@ class Tabs {
     setActiveHashTab() {
         let path = window.location.href.split('#')[1];
         // If the user goes back to the first tab with the back button,
-        // path will be undefined, so manually set it to tab-tab-1
+        // path will be undefined
         if (!path) {
-            path = 'tab-tab-1';
+            path = '';
         }
 
         this.allTabPanels.forEach((tabPanel) => {
