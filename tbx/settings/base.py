24fd69862c9f556bf287f7f230505c547ced19e3
Helen C <helen.chapman@torchbox.com>
2024-02-29 20:23:56 +0000

    Enforce WAGTAILIMAGES_MAX_UPLOAD_SIZE env var

--- tbx/settings/base.py
+++ tbx/settings/base.py
@@ -450,6 +450,9 @@ pixel_limit = env.get("WAGTAILIMAGES_MAX_IMAGE_PIXELS")
 WAGTAILIMAGES_MAX_IMAGE_PIXELS = int(pixel_limit) if pixel_limit else 10_000_000
 
 
+max_upload_size = env.get("WAGTAILIMAGES_MAX_UPLOAD_SIZE")
+WAGTAILIMAGES_MAX_UPLOAD_SIZE = int(max_upload_size) if max_upload_size else 2 * 1024 * 1024  # 2MB
+
 # Facebook JSSDK app Id
 FB_APP_ID = ""
 
