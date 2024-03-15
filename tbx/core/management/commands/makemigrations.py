99e8e859fcfcb4c892b0e01bd9691d7e55485605
Victor Miti <victor.miti@torchbox.com>
2024-03-15 08:59:15 +0000

    We don't need to monkey-patch django's management machinery

--- tbx/core/management/commands/makemigrations.py
+++
@@ -1,9 +0,0 @@
-"""
-Override of Django makemigrations.
-
-This ensure the patches in __init__ are picked up.
-"""
-
-from django.core.management.commands.makemigrations import Command
-
-__all__ = ["Command"]
--- tbx/core/management/commands/migrate.py
+++
@@ -1,7 +0,0 @@
-"""
-Overrides Django migrate.
-
-This ensure the patches in __init__ are picked up.
-"""
-
-from django.core.management.commands.migrate import Command  # noqa
