3e3b1e6bdda93a0549ed45ed8fa058e63de96022
Victor Miti <engineervix@users.noreply.github.com>
2024-04-15 15:29:04 +0100

    Integration - impact report (#195)
    * Copy pattern library config for impact report from current build
    
    * add impact_reports app
    
    this is copied from the current build
    
    * Add template placeholders for impact reports
    
    * We don't need impact_reports.ImpactReportAuthor because it's covered by torchbox.PageAuthor
    
    * Add database migration
    
    * Remove ParagraphWithQuoteBlock
    
    * Remove TwoColumnTextGridBlock
    
    * Remove ThreeColumnImageGridBlock
    
    * Remove ParagraphWithImageBlock
    
    * Use tbx.core.utils.fields.StreamField on ImpactReportPage
    
    * WIP: FE | Impact Report page styles (#197)
    
    * Rename instagram gallery block for consistency
    
    * Set up impact report page with dummy blocks
    
    * Add template class to homepage
    
    * Style impact report heading block
    
    * Remove impact report blocks that no longer exist from the impact report pattern library page
    
    * Update instagram block template path
    
    * Add instagram gallery markup and styling
    
    * Updates to impact report page fields to be in line with design
    
    * Don't lazy load the work page image as it is too near the top
    
    * Update template fields and display the main image
    
    * page heading area and anchor links
    
    * Update impact report main image to match the work page one
    
    * make the navigation sticky at desktop
    
    * further updates to the impact report model to add the common page mixins
    
    * isort
    
    * Correct anchor landing position and make nav sticky at mobile
    
    * Styling for small image with text
    
    * accessibility fixes
    
    * Make sure everything works in the pattern library and update the styleguide
    
    * fix class names
    
    * Make the impact report heading image a required field
    
    * Update docs
    
    * don't animate arrow in nav for prefers reduced motion
    
    * typo fix
    
    * squash migrations and code review feedback
    
    ---------
    
    Co-authored-by: Helen C <helen.chapman@torchbox.com>
    
    * Add authors to impact report (#206)
    
    * Add authors to the impact report, and rename the work-page-authors component now that it is used elsewhere
    
    * Update the pattern library
    
    * Catch another ref to work-page-authors
    
    * Update yaml to remove saturation-0.6
    
    ---------
    
    Co-authored-by: Sharmaine Lim <SharmaineLim@users.noreply.github.com>
    Co-authored-by: Helen C <helen.chapman@torchbox.com>

---
+++ tbx/impact_reports/blocks.py
@@ -0,0 +1,59 @@
+from tbx.core.blocks import StoryBlock
+from wagtail.blocks import (
+    CharBlock,
+    ListBlock,
+    RichTextBlock,
+    StructBlock,
+    URLBlock,
+)
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class ImpactReportHeadingBlock(StructBlock):
+    image = ImageChooserBlock()
+    short_heading = CharBlock(
+        required=False, help_text="Used for the Table of Contents"
+    )
+    heading = CharBlock(required=False)
+
+    class Meta:
+        icon = "title"
+        template = (
+            "patterns/molecules/streamfield/blocks/impact_report_heading_block.html"
+        )
+
+
+class SmallImageWithTextBlock(StructBlock):
+    image = ImageChooserBlock()
+    title = CharBlock()
+    text = RichTextBlock()
+
+    class Meta:
+        icon = "image"
+        template = (
+            "patterns/molecules/streamfield/blocks/small_image_with_text_block.html"
+        )
+
+
+class InstagramGalleryItemBlock(StructBlock):
+    image = ImageChooserBlock()
+    link = URLBlock(
+        required=False,
+    )
+
+    class Meta:
+        icon = "image"
+
+
+class InstagramGalleryGridBlock(StructBlock):
+    items = ListBlock(InstagramGalleryItemBlock())
+
+    class Meta:
+        icon = "group"
+        template = "patterns/molecules/streamfield/blocks/instagram_gallery_block.html"
+
+
+class ImpactReportStoryBlock(StoryBlock):
+    impact_report_heading = ImpactReportHeadingBlock(group="Impact Report")
+    small_image_with_text = SmallImageWithTextBlock(group="Impact Report")
+    instagram_gallery = InstagramGalleryGridBlock(group="Impact Report")
---
+++ tbx/impact_reports/models.py
@@ -0,0 +1,96 @@
+from django.db import models
+from django.utils.functional import cached_property
+from django.utils.text import slugify
+
+from tbx.core.utils.fields import StreamField
+from tbx.core.utils.models import ColourThemeMixin, SocialFields
+from tbx.impact_reports.blocks import ImpactReportStoryBlock
+from tbx.people.models import ContactMixin
+from wagtail.admin.panels import (
+    FieldPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    TitleFieldPanel,
+)
+from wagtail.models import Page
+from wagtail.search import index
+
+
+class ImpactReportPage(ColourThemeMixin, ContactMixin, SocialFields, Page):
+    template = "patterns/pages/impact_reports/impact_report_page.html"
+
+    hero_image = models.ForeignKey(
+        "images.CustomImage",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="This is for the illustration only. Use an image with dimensions of 571x700",
+    )
+
+    hero_caption = models.CharField("caption", max_length=255, blank=True)
+    hero_attribution = models.CharField("attribution", max_length=255, blank=True)
+
+    body = StreamField(ImpactReportStoryBlock(), use_json_field=True)
+
+    content_panels = [
+        MultiFieldPanel(
+            [
+                TitleFieldPanel("title"),
+                FieldPanel("hero_image"),
+                FieldPanel("hero_caption"),
+                FieldPanel("hero_attribution"),
+            ],
+            heading="Hero",
+        ),
+        MultiFieldPanel(
+            [
+                InlinePanel("authors", label="Author", min_num=1),
+            ],
+            heading="Introduction",
+        ),
+        FieldPanel("body"),
+    ]
+
+    promote_panels = (
+        [
+            MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        ]
+        + ColourThemeMixin.promote_panels
+        + ContactMixin.promote_panels
+        + [
+            MultiFieldPanel(SocialFields.promote_panels, "Social fields"),
+        ]
+    )
+
+    search_fields = Page.search_fields + [
+        index.SearchField("body"),
+    ]
+
+    @property
+    def headings(self):
+        """
+        Gets all of the impact report headers' short headings and their slugs,
+        including the Introduction.
+
+        This is used to create a table-of-contents like section at the top of
+        the page where viewers can jump to the top of each impact report heading.
+        """
+
+        headings = []
+
+        for block in self.body:
+            if block.block_type == "impact_report_heading":
+                headings.append(
+                    {
+                        "short_heading": block.value["short_heading"],
+                        "slug": slugify(block.value["short_heading"]),
+                    }
+                )
+        return headings
+
+    @cached_property
+    def first_author(self):
+        """Safely return the first author if one exists."""
+        if author := self.authors.first():
+            return author.author
+        return None
