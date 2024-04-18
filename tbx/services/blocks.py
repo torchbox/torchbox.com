64c0f316431c990dcee3215386a8ad57ae26fcb0
Helen Chapman <helen.chapman@torchbox.com>
2024-04-18 11:13:59 +0100

    Use motif headings for h2s on the service page, and indent the paragraph blocks (#217)
    * Use motif headings for h2s on the service page, and indent the paragraphs
    
    * run black
    
    * Tweak paragraph position after feedback from ben

--- tbx/services/blocks.py
+++ tbx/services/blocks.py
@@ -62,3 +62,17 @@ class ServiceStoryBlock(StoryBlock):
     tabbed_paragraph = TabbedParagraphBlock()
     event = EventBlock()
     values = ValuesBlock()
+    # redefine the heading 2 block from StoryBlock and use the motif heading template
+    h2 = blocks.CharBlock(
+        form_classname="title",
+        icon="title",
+        template="patterns/molecules/streamfield/blocks/heading2_motif_block.html",
+        group="Basics",
+    )
+    # redefine the paragraph block from StoryBlock to exclude the h2 option
+    paragraph = blocks.RichTextBlock(
+        icon="pilcrow",
+        template="patterns/molecules/streamfield/blocks/paragraph_block.html",
+        features=["h3", "h4", "bold", "italic", "ul", "ol", "link", "document-link"],
+        group="Basics",
+    )
