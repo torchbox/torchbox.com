a45bbe33110951e686d293eb9762bf2902400697
Helen C <helen.chapman@torchbox.com>
2024-04-23 15:06:09 +0100

    Code review feedback on tests

---
+++ tbx/core/tests/test_generic_page_functionality.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from tbx.core.models import StandardPage
+
+
+class TestNavigationText(SimpleTestCase):
+
+    def test_nav_text_prefers_navigation_text(self):
+        page = StandardPage(id=1, title="Title", navigation_text="Navigation text")
+        self.assertEqual(page.nav_text, "Navigation text")
+
+    def test_nav_text_uses_page_title_as_fallback(self):
+        page = StandardPage(id=1, title="Title", navigation_text="")
+        self.assertEqual(page.nav_text, "Title")
