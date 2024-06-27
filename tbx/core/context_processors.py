1e70dbd054ff15f043a10b4e24cb6328d728f999
Helen C <helen.chapman@torchbox.com>
2024-06-27 12:40:39 +0100

    JS implementation of the site switcher
    - The cookie is read on the serverside to avoid a FOUC
    - Based on the RNIB implementation

--- tbx/core/context_processors.py
+++ tbx/core/context_processors.py
@@ -1,8 +1,18 @@
+import json
 from django.conf import settings
 
 
 def global_vars(request):
+
+    # Read the mode cookie to determine the user's saved preference for light or dark mode, if it exists
+    # Ensure it is one of the allowed values
+    mode = request.COOKIES.get("mode", "dark")
+    if mode not in settings.ALLOWED_MODES:
+        mode = "dark"
+
     return {
         "GOOGLE_TAG_MANAGER_ID": getattr(settings, "GOOGLE_TAG_MANAGER_ID", None),
         "SEO_NOINDEX": settings.SEO_NOINDEX,
+        "ALLOWED_MODES": json.dumps(settings.ALLOWED_MODES),
+        "MODE": mode,
     }
