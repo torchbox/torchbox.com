999168e0048f9632899410155b309852ed8bec91
Helen C <helen.chapman@torchbox.com>
2024-07-22 14:52:45 +0100

    black

--- tbx/core/context_processors.py
+++ tbx/core/context_processors.py
@@ -7,6 +7,10 @@ def global_vars(request):
     return {
         "GOOGLE_TAG_MANAGER_ID": getattr(settings, "GOOGLE_TAG_MANAGER_ID", None),
         "SEO_NOINDEX": settings.SEO_NOINDEX,
-        "COOKIE_POLICY_PAGE": ImportantPageSettings.for_request(request).cookie_policy_page,
-        "CARBON_EMISSIONS_PAGE": ImportantPageSettings.for_request(request).carbon_emissions_page,
+        "COOKIE_POLICY_PAGE": ImportantPageSettings.for_request(
+            request
+        ).cookie_policy_page,
+        "CARBON_EMISSIONS_PAGE": ImportantPageSettings.for_request(
+            request
+        ).carbon_emissions_page,
     }
