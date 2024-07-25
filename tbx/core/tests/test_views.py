b2c4fadae0a8953ddde7ce36bcc71da116377cce
Jake Howard <jake.howard@torchbox.com>
2024-07-25 16:29:21 +0100

    Use simpler error pages when HTML isn't needed
    This saves queries and processing time, if the HTML isn't ever going to be rendered, and simple text would be enough.

--- tbx/core/tests/test_views.py
+++ tbx/core/tests/test_views.py
@@ -106,3 +106,28 @@ class TestModeSwitcherView(TestCase):
         # check theme is set on new site
         resp = self.client.get(f"http://{new_site.hostname}/")
         self.assertEqual(resp.context["MODE"], mode)
+
+class PageNotFoundTestCase(TestCase):
+    url = "/does-not-exist/"
+
+    def test_accessible(self) -> None:
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 404)
+        self.assertIn("text/html", response.headers["content-type"])
+
+    def test_accept_html(self) -> None:
+        response = self.client.get(self.url, headers={"Accept": "text/html"})
+        self.assertEqual(response.status_code, 404)
+        self.assertIn("text/html", response.headers["content-type"])
+
+    def test_simple_when_doesnt_accept_html(self) -> None:
+        response = self.client.get(self.url, headers={"Accept": "text/css"})
+        self.assertEqual(response.status_code, 404)
+        self.assertIn("text/plain", response.headers["content-type"])
+
+    def test_simple_when_html_not_highest(self) -> None:
+        response = self.client.get(
+            self.url, headers={"Accept": "text/html;q=0.8,text/css"}
+        )
+        self.assertEqual(response.status_code, 404)
+        self.assertIn("text/plain", response.headers["content-type"])
--- tbx/core/utils/views.py
+++ tbx/core/utils/views.py
@@ -1,9 +1,59 @@
+from django.http import HttpResponseNotFound, HttpResponseServerError
 from django.views import defaults
+from django.views.decorators.csrf import requires_csrf_token
+from django.views.decorators.vary import vary_on_headers
 
 
-def page_not_found(request, exception, template_name="patterns/pages/errors/404.html"):
-    return defaults.page_not_found(request, exception, template_name)
+def get_quality(media_type):
+    return float(media_type.params.get("q", 1))
 
 
+def show_html_error_page(request):
+    accepted_types = sorted(request.accepted_types, key=get_quality, reverse=True)
+
+    html_type = next(
+        (
+            accepted_type
+            for accepted_type in accepted_types
+            if accepted_type.match("text/html")
+        ),
+        None,
+    )
+
+    # If HTML isn't accepted, don't serve it
+    if html_type is None:
+        return False
+
+    max_quality = get_quality(accepted_types[0])
+
+    # If HTML isn't the highest quality, don't serve it
+    if get_quality(html_type) < max_quality:
+        return False
+
+    return True
+
+
+@vary_on_headers("Accept")
+@requires_csrf_token
+def page_not_found(
+    request, exception=None, template_name="patterns/pages/errors/404.html"
+):
+    if show_html_error_page(request):
+        return defaults.page_not_found(request, exception, template_name)
+
+    # Serve a simpler, cheaper 404 page if we don't need to
+    return HttpResponseNotFound(
+        "Page not found", content_type="text/plain; charset=utf-8"
+    )
+
+
+@vary_on_headers("Accept")
+@requires_csrf_token
 def server_error(request, template_name="patterns/pages/errors/500.html"):
-    return defaults.server_error(request, template_name)
+    if show_html_error_page(request):
+        return defaults.server_error(request, template_name)
+
+    # Serve a simpler, cheaper 500 page if we don't need to
+    return HttpResponseServerError(
+        "Server error", content_type="text/plain; charset=utf-8"
+    )
