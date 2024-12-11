fd0c6a115406c86582fed90dd54b48ca6fa1a420
zerolab <dan@zerolab.org>
2024-12-11 19:48:37 +0000

    Run pypgrade with py312-plus
    `git ls-files --others --cached --exclude-standard -- '*.py' | xargs pyupgrade --py312-plus`

--- tbx/blog/feeds.py
+++ tbx/blog/feeds.py
@@ -38,7 +38,7 @@ class BlogFeed(Feed):
     def item_enclosure_mime_type(self, item):
         if item.feed_image:
             image_format = imghdr.what(item.feed_image.file)
-            return "image/{}".format(image_format)
+            return f"image/{image_format}"
 
     def item_enclosure_length(self, item):
         if item.feed_image:
