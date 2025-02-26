598cd7aff8678d6a8c4a48caa7b6e251ae95a656
Amanda Lim-Cabuloy <amanda.lim@torchbox.com>
2025-02-26 18:50:14 +0800

    Add data migration

--- tbx/divisions/migrations/0003_remove_divisionpage_label_divisionpage_logo.py
+++ tbx/divisions/migrations/0003_remove_divisionpage_label_divisionpage_logo.py
@@ -3,16 +3,42 @@
 from django.db import migrations, models
 
 
+def label_to_logo(apps, schema_editor):
+    DivisionPage = apps.get_model("divisions", "DivisionPage")
+
+    DivisionPage.objects.update(
+        logo=models.Case(
+            models.When(label="Charity", then=models.Value("logo-charity")),
+            models.When(label="Public", then=models.Value("logo-public")),
+            models.When(
+                label="Wagtail CMS services", then=models.Value("logo-wagtail")
+            ),
+            default=models.Value("logo-torchbox"),
+        )
+    )
+
+
+def logo_to_label(apps, schema_editor):
+    DivisionPage = apps.get_model("divisions", "DivisionPage")
+
+    DivisionPage.objects.update(
+        logo=models.Case(
+            models.When(label="logo-charity", then=models.Value("Charity")),
+            models.When(label="logo-public", then=models.Value("Public")),
+            models.When(
+                label="logo-wagtail", then=models.Value("Wagtail CMS services")
+            ),
+            default=models.Value(""),
+        )
+    )
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("divisions", "0002_divisionmixin_and_navigationsetmixin"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name="divisionpage",
-            name="label",
-        ),
         migrations.AddField(
             model_name="divisionpage",
             name="logo",
@@ -26,5 +52,10 @@ class Migration(migrations.Migration):
                 default="logo-torchbox",
                 max_length=50,
             ),
+        ),
+        migrations.RunPython(label_to_logo, logo_to_label),
+        migrations.RemoveField(
+            model_name="divisionpage",
+            name="label",
         ),
     ]
