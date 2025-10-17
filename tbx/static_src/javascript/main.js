c29aeb7698e2f664356b2288a7265c12b82c0524
Jakub Mastalerz <jakub.mastalerz@torchbox.com>
2025-10-17 14:12:33 +0100

    Add cookie check for VWO script loading

--- tbx/static_src/javascript/main.js
+++ tbx/static_src/javascript/main.js
@@ -9,6 +9,7 @@ import YouTubeConsentManager from './components/youtube-embed';
 import Tabs from './components/tabs';
 import TableHint from './components/table-hint';
 import ModeSwitcher from './components/mode-switcher';
+import VWOLoader from './components/vwo-loader';
 
 // IE11 polyfills
 import foreachPolyfill from './polyfills/foreach-polyfill';
@@ -40,4 +41,5 @@ document.addEventListener('DOMContentLoaded', () => {
     initComponent(TableHint);
     initComponent(ModeSwitcher);
     new DesktopCloseMenus();
+    new VWOLoader();
 });
